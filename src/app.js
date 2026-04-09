b4341fd9c553a24183413071ae8b4e04d5791078
Matias Araneda <maty.araneda@gmail.com>
2026-04-08 23:07:30 -0400

    se ajusta cors para trabajar con front y front debug

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from "cors";
+
+
+import { ORIGIN_CORS } from "./config.js";
 
 //rutas del proyecto
 import PostRouter from './routes/posts.routes.js';
@@ -10,6 +14,19 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(helmet());
 
+const allowedOrigins = ORIGIN_CORS();
+app.use(    
+  cors({
+    origin: function (origin, callback) {
+        console.log("origin=>", origin);
+        if (!origin || allowedOrigins.includes(origin)) return callback(null, origin);
+        else return callback("Error de CORS origin: " + origin + " No autorizado!");
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+})
+);
+
 app.disable('x-powered-by');
 
 app.use("/api", PostRouter);
@@ -18,7 +35,7 @@ app.use("/api", PostRouter);
 // =====================custom 404
 app.use((req, res, next) => {
     let response = `
-        <div>El recurso no fue encontrado y no es mi culpa.</div>
+        <div>El recurso no fue encontrado</div>
     `;
 
     res.status(404).send(response)
@@ -27,7 +44,7 @@ app.use((req, res, next) => {
   // ===============custom error handler
   app.use((err, req, res, next) => {
     let response = `
-        <div>El servidor esta bien Larry y no es mi culpa.</div>
+        <div>El servidor esta off.</div>
     `;
     res.status(500).send(response)
   })
--- src/config.js
+++ src/config.js
@@ -10,3 +10,14 @@ export default
     db_port: process.env.DB_PORT,
     db_database: process.env.DB_DATABASE,
 }
+
+export const ORIGIN_CORS = () => {
+    const salida = [];
+    let arreglo = process.env.ORIGIN
+    arreglo.split(',').forEach((fila) => {
+        let direccion = fila.toString()
+        direccion = direccion.toString().slice(1,-1)
+        salida.push(direccion)
+    })
+    return salida;
+}
